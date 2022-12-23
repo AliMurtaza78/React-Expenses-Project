116dce8aba26a4766d1faec41d0f81f6d16b5767
Ali Murtaza <syedalim9087@gmail.com>
2022-12-23 11:55:58 +0100

    Added filtered and conditional functionality to the Expenses List Component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,6 +11,23 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No Expenses Found</p>;
+
+  if(filteredExpenses.length > 0){
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -18,14 +35,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onFilterHandler={filterChangeHandler}
         />
-        {props.items.map((expense) => (
-          <ExpenseItem
-          key ={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
